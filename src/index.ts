a98427325acb9c9945927490e18c9dcfaa4b23b0
Joseph Bartley <joseph@josephbartley.dev>
2022-09-14 00:03:25 -0500

    refactor: use axios instead of simple-oauth2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import axios, { AxiosError, AxiosResponse } from 'axios'
-import { AccessToken, ClientCredentials, ModuleOptions } from 'simple-oauth2'
 
 export interface AirThingsConfiguration {
 	id: string
 	secret: string
+}
+
+export interface AccessToken {
+	token: string
+	type: string
+	expiresAt: number
 }
 
 export interface Device {
@@ -70,25 +75,11 @@ export interface Readings {
  * AirThings API client for Node.js
  */
 export class AirThingsApi {
-	private config: ModuleOptions
+	private config: AirThingsConfiguration
 	private accessToken: AccessToken | null
 
 	constructor(configuration: AirThingsConfiguration) {
-		this.config = {
-			client: {
-				id: configuration.id,
-				secret: configuration.secret,
-			},
-			auth: {
-				tokenHost: 'https://accounts.airthings.com',
-				tokenPath: 'https:/accounts-api.airthings.com/v1/token',
-				authorizePath: '/authorize',
-			},
-			options: {
-				authorizationMethod: 'body',
-			},
-		}
-
+		this.config = configuration
 		this.accessToken = null
 	}
 
@@ -97,16 +88,13 @@ export class AirThingsApi {
 	 * @returns {Device[]}
 	 */
 	public async getDeviceList(): Promise<Device[]> {
-		if (!this.accessToken || this.accessToken.expired()) {
-			await this.updateToken()
-		}
+		if (this.isTokenExpired()) await this.updateToken()
+		if (!this.accessToken) throw new Error('No access token')
 
 		return await axios
 			.get('https://ext-api.airthings.com/v1/devices', {
 				headers: {
-					Authorization: `Bearer ${
-						this.accessToken?.token['access_token'] as string
-					}`,
+					Authorization: `Bearer ${this.accessToken.token}`,
 				},
 			})
 			.then((value: AxiosResponse<{ devices: Device[] }>) => {
@@ -123,16 +111,13 @@ export class AirThingsApi {
 	 * @returns {Device}
 	 */
 	public async getDevice(deviceId: string): Promise<Device> {
-		if (!this.accessToken || this.accessToken.expired()) {
-			await this.updateToken()
-		}
+		if (this.isTokenExpired()) await this.updateToken()
+		if (!this.accessToken) throw new Error('No access token')
 
 		return await axios
 			.get(`https://ext-api.airthings.com/v1/devices/${deviceId}`, {
 				headers: {
-					Authorization: `Bearer ${
-						this.accessToken?.token['access_token'] as string
-					}`,
+					Authorization: `Bearer ${this.accessToken.token}`
 				},
 			})
 			.then((value: AxiosResponse) => {
@@ -149,18 +134,15 @@ export class AirThingsApi {
 	 * @returns {Readings}
 	 */
 	public async getDeviceSamples(deviceId: string): Promise<Readings> {
-		if (!this.accessToken || this.accessToken.expired()) {
-			await this.updateToken()
-		}
+		if (this.isTokenExpired()) await this.updateToken()
+		if (!this.accessToken) throw new Error('No access token')
 
 		return await axios
 			.get(
 				`https://ext-api.airthings.com/v1/devices/${deviceId}/latest-samples`,
 				{
 					headers: {
-						Authorization: `Bearer ${
-							this.accessToken?.token['access_token'] as string
-						}`,
+						Authorization: `Bearer ${this.accessToken.token}`,
 					},
 				},
 			)
@@ -173,19 +155,46 @@ export class AirThingsApi {
 	}
 
 	/**
+	 * Checks if the access token is expired (or nearly expired)
+	 * @returns {boolean}
+	 */
+	private isTokenExpired(): boolean {
+		if (!this.accessToken) return true
+
+		return this.accessToken.expiresAt - 15 < Date.now()
+	}
+
+	/**
 	 * Gets the access token from the API
 	 */
 	private async updateToken(): Promise<void> {
-		const client = new ClientCredentials(this.config)
-		this.accessToken = await client
-			.getToken({ scope: 'read:device:current_value' })
-			.then((value: AccessToken) => {
-				return value
-			})
-			.catch((error: Error | AxiosError) => {
-				throw this.getError(error)
-			})
+		const body = JSON.stringify({
+			grant_type: 'client_credentials',
+		})
+
+		this.accessToken = await axios.post('https://accounts-api.airthings.com/v1/token', body, {
+			headers: {
+				'Authorization': `Basic ${Buffer.from(`${this.config.id}:${this.config.secret}`).toString('base64')}`,
+				'content-type': 'application/json',
+			},
+		},
+		).then((value: AxiosResponse<{
+			access_token: string,
+			token_type: string,
+			expires_in: number,
+		}>) => {
+			console.log(value.data)
+			const token: AccessToken = {
+				token: value.data.access_token,
+				type: value.data.token_type,
+				expiresAt: value.data.expires_in + Date.now(),
+			}
+			return token
+		}).catch((error: Error | AxiosError) => {
+			throw this.getError(error)
+		})
 	}
+
 
 	private getError(error: Error | AxiosError): Error {
 		if (axios.isAxiosError(error)) {
