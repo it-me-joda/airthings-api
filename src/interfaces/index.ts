d6390430a3e8fcabe1b98abb6d64ca87dbc024a7
Joseph Bartley <joseph@josephbartley.dev>
2023-10-06 09:45:20 -0500

    refactor: move interfaces to own folder

---
+++ src/interfaces/index.ts
@@ -0,0 +1,4 @@
+export type { AccessToken } from './access-token'
+export type { AirThingsConfiguration } from './airthings-configuration'
+export type { Device } from './device'
+export type { Readings } from './readings'
