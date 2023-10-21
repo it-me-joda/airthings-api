1890b96f2c2d9951a97f5cec1e2bce27a32f0410
Joseph Bartley <josephdanielbartley@gmail.com>
2023-10-21 00:29:21 -0500

    feat: add query parameters

--- src/interfaces/location.ts
+++ src/interfaces/location.ts
@@ -1,35 +1,35 @@
-import { Device, DeviceSegment } from "./device"
-import { Samples } from "./samples"
+import { Device, DeviceSegment } from './device'
+import { Samples } from './samples'
 
 export interface Location {
-    id: string
-    name: string
+	id: string
+	name: string
 }
 
 export interface LocationInfo extends Location {
-    labels: object
-    devices: Device[]
-    lat: number
-    lng: number
-    address?: string 
-    buildingHeight?: number
-    buildingSize?: number
-    buildingType?: string
-    buildingVolume?: number
-    buildingYear?: number
-    countryCode?: string
-    floors?: number
-    timezone?: string
-    usageHours?: object
-    ventilationType?: object
+	labels: object
+	devices: Device[]
+	lat: number
+	lng: number
+	address?: string
+	buildingHeight?: number
+	buildingSize?: number
+	buildingType?: string
+	buildingVolume?: number
+	buildingYear?: number
+	countryCode?: string
+	floors?: number
+	timezone?: string
+	usageHours?: object
+	ventilationType?: object
 }
 
 export interface LocationSamples extends Location {
-    id: string
-    name: string
-    devices: {
-        id: string
-        data: Samples
-        segment: DeviceSegment
-    }[]
+	id: string
+	name: string
+	devices: {
+		id: string
+		data: Samples
+		segment: DeviceSegment
+	}[]
 }
